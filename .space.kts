2ac7e5369759c6a4d5a57239d428b6b0a0aef2da
sirius_black9999 <sirius_black9999@hotmail.com>
2023-10-19 23:13:13 +0200

    trying to set up build script

--- .space.kts
+++ .space.kts
@@ -16,32 +16,28 @@ job("run tests") {
         // get the entire commit history
         depth = UNLIMITED_DEPTH
     }
-val sharedVolume = volume("shared-volume")
-
-job("Build and Test Rust and C# Projects") {
-  // Container for Rust
-  container(displayName = "Rust Container", image = "rust:latest") {
-    volumeMount(sharedVolume, "/shared")
-    shellScript {
-      content = """
-      cd shadow_rust
-      cargo build --release --verbose
-      cargo test --verbose
-      cp target/release/mylib.so /shared/
-      """
-    }
-  }
-
-  // Container for C#
-  container(displayName = "C# Container", image = "mcr.microsoft.com/dotnet/sdk:latest") {
-    volumeMount(sharedVolume, "/shared")
-    shellScript {
-      content = """
-      cp /shared/mylib.so MyCSProject/libs/
-      dotnet restore
-      dotnet test
-      """
-    }
-  }
+    // Container for Rust
+      container(displayName = "Rust Container", image = "rust:latest") {
+        shellScript {
+          content = """
+          cd shadow_rust
+          cargo build --release --verbose
+          """
+        }
+        // Uploading the Rust library as an artifact
+        artifactRules = listOf("shadow_rust/target/release/*.so")
+      }
+      
+      // Container for C#
+      container(displayName = "C# Container", image = "mcr.microsoft.com/dotnet/sdk:latest") {
+        // Downloading the Rust library from artifacts
+        artifactRules = listOf("*.so => MyCSProject/libs/")
+        shellScript {
+          content = """
+          dotnet restore
+          dotnet test
+          """
+        }
+      }
 }
 }
