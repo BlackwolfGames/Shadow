8b1311ce6aaa57a7245634d0b78768a08e7c7fa7
sirius_black9999 <sirius_black9999@hotmail.com>
2023-10-19 23:13:13 +0200

    trying to set up build script

--- .space.kts
+++ .space.kts
@@ -16,13 +16,32 @@ job("run tests") {
         // get the entire commit history
         depth = UNLIMITED_DEPTH
     }
+val sharedVolume = volume("shared-volume")
 
-    container(displayName = "Rust Container", image = "rust:latest") {
-      shellScript {
-        content = """
-        cargo build --verbose
-        cargo test --verbose
-        """
-      }
+job("Build and Test Rust and C# Projects") {
+  // Container for Rust
+  container(displayName = "Rust Container", image = "rust:latest") {
+    volumeMount(sharedVolume, "/shared")
+    shellScript {
+      content = """
+      cd shadow_rust
+      cargo build --release --verbose
+      cargo test --verbose
+      cp target/release/mylib.so /shared/
+      """
     }
+  }
+
+  // Container for C#
+  container(displayName = "C# Container", image = "mcr.microsoft.com/dotnet/sdk:latest") {
+    volumeMount(sharedVolume, "/shared")
+    shellScript {
+      content = """
+      cp /shared/mylib.so MyCSProject/libs/
+      dotnet restore
+      dotnet test
+      """
+    }
+  }
+}
 }
