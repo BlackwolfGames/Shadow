d2c5d1f14e10f8461aff83036dcd785341db438d
sirius_black9999 <sirius_black9999@hotmail.com>
2023-10-20 23:34:24 +0200

    Adding analyser stuffs and more build script tweaking

---
+++ SourceVisSpec/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace SourceVisSpec.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ SourceVisSpec/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SourceVisSpec.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
