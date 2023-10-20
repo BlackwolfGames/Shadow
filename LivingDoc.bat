d149e5104062b07740f41c61ba93678af53d3520
sirius_black9999 <sirius_black9999@hotmail.com>
2023-10-20 21:54:16 +0200

    more messing with the build script...

--- LivingDoc.bat
+++ LivingDoc.bat
@@ -1,4 +1,4 @@
-cd "ShadowSpecs\bin\Debug\net6.0"
+cd "ShadowSpecs\bin\Debug\net7.0"
 livingdoc test-assembly ShadowSpecs.dll -t TestExecution.json
 copy "LivingDoc.html" "../../../../LivingDoc.html"
 start LivingDoc.html
