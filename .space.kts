1c692249a1a33af5e8cdfd9762d81e03c38c2d4c
sirius_black9999 <sirius_black9999@hotmail.com>
2023-10-21 20:57:18 +0200

    working on the giga script for weekly static analysis

--- .space.kts
+++ .space.kts
@@ -4,9 +4,104 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("run tests") {
+job("run tests on commit") {
     startOn {
+        gitPush { enabled = false }
+	}
+  git {
+        // fetch 'release' branch and tags
+        refSpec {
+            +"Devel"
+        }
+        // get the entire commit history
+        depth = UNLIMITED_DEPTH
+    }
+    // Container for Rust
+      container(displayName = "Rust Container", image = "rust:latest") {
+        shellScript {
+          content = """
+          apt-get update
+          apt-get install -y clang lld mingw-w64
+          rustup target add x86_64-pc-windows-gnu
+              
+          cd shadow_rust
+          
+          mkdir artifacts
+          cd rusty_brain
+          cargo build --release --verbose --package rusty_brain --target=x86_64-pc-windows-gnu
+          cargo test --color=always --package rusty_brain --lib tests --no-fail-fast -- --show-output
+          
+          ls
+          cd target 
+          ls
+          cd x86_64-pc-windows-gnu 
+          ls
+          cd release
+          ls
+          
+          
+          cp rusty_brain.dll ../../../../artifacts/rusty_brain.dll
+          """
+        }
+
+          // Upload build/build.zip to the default file repository
+          fileArtifacts {
+              // To upload to another repo, uncomment the next line
+              // repository = FileRepository(name = "my-file-repo", remoteBasePath = "{{ run:number }}")
+
+              // Local path to artifact relative to working dir
+              localPath = "shadow_rust/artifacts/rusty_brain.dll"
+              // Don't fail job if build.zip is not found
+              optional = false
+              // Target path to artifact in file repository.
+              remotePath = "rusty_brain.dll"
+              // Upload condition (job run result): SUCCESS (default), ERROR, ALWAYS
+              onStatus = OnStatus.SUCCESS
+          }
+      }
+      
+      // Container for C#
+      container(displayName = "C# Container", image = "mcr.microsoft.com/dotnet/sdk:latest") {
+
+          fileInput {
+              // we use the provided parameter to reference the default repo
+              source = FileSource.FileArtifact(
+                      "{{ run:file-artifacts.default-repository }}/Shadow/jobs/run-tests/{{ run:number }}",
+                      "rusty_brain.dll"
+              )
+              localPath = "RustLib/rusty_brain.dll"
+          }
+
+        shellScript {
+            content = """              
+              # Building and running tests from Analyzers1.Tests
+              dotnet build Analyzers1/Analyzers1.Tests/Analyzers1.Tests.csproj
+              dotnet test Analyzers1/Analyzers1.Tests/Analyzers1.Tests.csproj  --logger "trx;LogFileName=results.trx"
+              
+              # Building and running tests from SourceVisUnit
+              dotnet build SourceVisUnit/SourceVisUnit.csproj
+              dotnet test SourceVisUnit/SourceVisUnit.csproj  --logger "trx;LogFileName=results.trx"
+              
+              # Building and running tests from SourceVisSpec
+              dotnet build SourceVisSpec/SourceVisSpec.csproj
+              dotnet test SourceVisSpec/SourceVisSpec.csproj  --logger "trx;LogFileName=results.trx"
+              
+              # Building and running tests from ShadowTest
+              dotnet build ShadowTest/ShadowTest.csproj
+              dotnet test ShadowTest/ShadowTest.csproj  --logger "trx;LogFileName=results.trx"
+              
+              # Building and running tests from ShadowSpecs
+              dotnet build ShadowSpecs/ShadowSpecs.csproj
+              dotnet test ShadowSpecs/ShadowSpecs.csproj  --logger "trx;LogFileName=results.trx"
+              """
+        }
+    }
+}
+job("Weekly stress test") {
+    startOn {
+    
         gitPush { enabled = true }
+        schedule { cron("0 0 * * 6") }
 	}
   git {
         // fetch 'release' branch and tags
@@ -76,7 +171,8 @@ job("run tests") {
             content = """
             
               # Installing livingdoc and specflow tools via dotnet
-              dotnet tool install --global SpecFlow.Plus.LivingDoc.CLI
+              dotnet tool install -g SpecFlow.Plus.LivingDoc.CLI
+              dotnet tool install -g dotnet-stryker
               export PATH="${'$'}PATH:/root/.dotnet/tools"
               
               mkdir artifacts
@@ -90,21 +186,42 @@ job("run tests") {
               dotnet build Analyzers1/Analyzers1.Tests/Analyzers1.Tests.csproj
               dotnet test Analyzers1/Analyzers1.Tests/Analyzers1.Tests.csproj  --logger "trx;LogFileName=results.trx"
               
+              cd Analyzers1/Analyzers1.Tests
+              dotnet stryker
+              cd ../../
+              
               # Building and running tests from SourceVisUnit
               dotnet build SourceVisUnit/SourceVisUnit.csproj
               dotnet test SourceVisUnit/SourceVisUnit.csproj  --logger "trx;LogFileName=results.trx"
+              
+              cd SourceVisUnit
+              dotnet stryker
+              cd ../
               
               # Building and running tests from SourceVisSpec
               dotnet build SourceVisSpec/SourceVisSpec.csproj
               dotnet test SourceVisSpec/SourceVisSpec.csproj  --logger "trx;LogFileName=results.trx"
               
+              cd SourceVisSpec
+              dotnet stryker
+              cd ../
+              
               # Building and running tests from ShadowTest
               dotnet build ShadowTest/ShadowTest.csproj
               dotnet test ShadowTest/ShadowTest.csproj  --logger "trx;LogFileName=results.trx"
               
+              cd ShadowTest
+              dotnet stryker
+              cd ../
+              
               # Building and running tests from ShadowSpecs
               dotnet build ShadowSpecs/ShadowSpecs.csproj
               dotnet test ShadowSpecs/ShadowSpecs.csproj  --logger "trx;LogFileName=results.trx"
+              
+              cd ShadowSpecs
+              dotnet stryker
+              cd ../    
+              
               
               dotnet build Analyzers1/Analyzers1.Tests/Analyzers1.csproj -c Release -r win-x64 --self-contained true -o /artifacts/Analysis
               dotnet build ShadowEngine/ShadowEngine.csproj -c Release -r win-x64 --self-contained true -o /artifacts/Shadow
