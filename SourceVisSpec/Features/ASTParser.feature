5a1ccf9561a2e8f0968ef32f089b721b16430e1f
sirius_black9999 <sirius_black9999@hotmail.com>
2023-10-22 23:23:09 +0200

    #MAIN-T-11 #MAIN-T-12 first testcase

---
+++ SourceVisSpec/Features/ASTParser.feature
@@ -0,0 +1,17 @@
+﻿Feature: Abstract syntax tree parser
+Scenario: Parser can read classes
+	Given we parse the following code
+	"""
+       namespace HelloWorld
+       {
+           class Program
+           {
+               static void Main(string[] args)
+               {
+                   Console.WriteLine("Hello, World!");
+               }
+           }
+       }
+	"""
+	Then there is 1 class
+	And The class 'HelloWorld.Program' depends on 'Console' 1 time 
---
+++ SourceVisSpec/Steps/ParsingSteps.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SourceVisCore.AST;
+
+namespace SourceVisSpec.Steps;
+
+[Binding]
+public class ParsingSteps
+{
+    private Project _parsed;
+
+    [Given(@"we parse the following code")]
+    public async Task GivenWeParseTheFollowingCode(string sourceCode)
+    {
+        _parsed = await Parser.ParseFromSource(sourceCode);
+    }
+
+    [Then(@"there is (.*) class")]
+    public void ThenThereIsClass(int p0)
+    {
+        Assert.That(_parsed.Classes.Count, Is.EqualTo(1));
+    }
+
+    private Func<KeyValuePair<string,T>, bool> ContainsKey<T>(string key)
+    {
+        return pair => Equals(pair.Key, key);
+    }
+    [Then(@"The class '(.*)' depends on '(.*)' (.*) times?")]
+    public void ThenTheClassDependsOnTimes(string className, string dependencyName, int dependencyCount)
+    {
+        Assert.That(_parsed.Classes.Select(pair => pair.Key), Contains.Item(className));
+        Assert.That(_parsed.Classes.First(ContainsKey<Class>(className)).Value.Dependencies.Select(pair => pair.Key), Contains.Item(dependencyName));
+        Assert.That(
+            _parsed.Classes.First(ContainsKey<Class>(className)).Value
+                .Dependencies.First(ContainsKey<int>(dependencyName)).Value,
+            Is.EqualTo(dependencyCount));
+    }
+}
