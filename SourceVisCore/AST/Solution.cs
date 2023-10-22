56bfa8ac9793743e803924b3ef9dcdfe5830ba31
sirius_black9999 <sirius_black9999@hotmail.com>
2023-10-22 20:58:45 +0200

    #MAIN-T-11 #MAIN-T-12 initial setup for project parsing
    still needs some testing though

---
+++ SourceVisCore/AST/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace SourceVisCore.AST;
+
+public class Solution
+{
+    private readonly Dictionary<string, Project> _projects = new(); 
+
+    public Project AddProject(string name)
+    {
+        _projects.TryAdd(name, new Project());
+        return _projects[name];
+    }
+
+    public void Print()
+    {
+        foreach (var project in _projects)
+        {
+            Console.WriteLine($"Project: {project.Key}");
+            project.Value.Print();
+        }
+    }
+}
